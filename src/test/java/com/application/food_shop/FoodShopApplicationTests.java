603cee0e6977314c0d0f80d37297668794f26cb3
wellington.nodari <wellington.nodari@visma.com>
2026-07-14 13:00:32 +0100

    Initial commit

---
+++ src/test/java/com/application/food_shop/FoodShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.food_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
